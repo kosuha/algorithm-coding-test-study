034772ddd054a79d811ed172d1dc0ef9b530307c
Seonho Kim <okeydokekim@gmail.com>
2021-03-14 13:14:28 +0900

    210314

--- K번째수.js
+++ K번째수.js
@@ -1,6 +1,8 @@
 const array = [1, 5, 2, 6, 3, 7, 4];
 const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+
 solution(array, commands);
+
 function solution(array, commands) {
     let result = [];
     for (let i = 0; i < commands.length; i++) {
