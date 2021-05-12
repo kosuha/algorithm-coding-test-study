b38c01b89b12f3cd271f8cd570d8c2ed516ec295
Seonho Kim <okeydokekim@gmail.com>
2021-05-12 20:39:35 +0900

    210512

--- Python/124 나라의 숫자.py
+++ Python/124 나라의 숫자.py
@@ -2,46 +2,55 @@ import math
 
 def solution(n):
     answer = ''
+    
+    num = get_ternary_reversed(n)
 
     while True:
-        mok = math.floor(n / 3)
-        
-        remain = n % 3
-        i += 1
-        if remain == 0:
-            r = 4
+        if num[0] == '0':
+            num = '4' + get_ternary_reversed(int(num[1::-1], base = 3) - 1)
+
+        answer = num[0] + answer
+
+        if len(num) > 1:
+            num = num[1:]
         else:
-            r = remain
-
-        answer = str(r) + answer
-        n = mok
-
-        if mok == 1 and remain == 0:
             break
-        if mok == 0:
-            break
-
+    
     print(answer)
-
     return answer
 
+def get_ternary_reversed(n):
+    result = ''
 
-solution(24)
+    while n >= 1:
+        n, rest = divmod(n, 3)
+        result += str(rest)
+    
+    return result
 
-# 3 9 27
-# ^1 ^2 ^3/ 
 
-#     21
-#     22 
-# 9  24
 
-# 10 41
-# 11 42
-# 12 44 3*3 + 3
+solution(6)
 
-# 13 111 3*3*
-# 14 112
-#     114
+# 1  1  1
+# 2  2  2
+# 3  4  10
+
+# 4  11 11
+# 5  12 12
+# 6  14 20
+
+# 7  21 21
+# 8  22 22
+# 9  24 100
+
+# 10 41 101
+# 11 42 102
+# 12 44 110
+
+# 13 111 111
+# 14 112 112
+# 15 114 120
 
 #     121
 #     122
@@ -49,11 +58,11 @@ solution(24)
 
 #     141
 #     142
-# 21    144
+# 21  144
 
 #     211
 #     212
-# 24    214
+# 24  214
 
 #     221
 #     222
