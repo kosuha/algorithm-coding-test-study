523641e429cedbe5d17a8c310dcc3b16c3af80c9
Seonho Kim <okeydokekim@gmail.com>
2021-06-01 13:29:25 +0900

    210601

--- lecture/note.py
+++ lecture/note.py
@@ -15,3 +15,10 @@ if any(x < 4 for x in a):
     print("a가 하나라도 조건에 맞을 경우 참")
 else:
     print("하나도 안 맞으면 거짓")
+
+# lambda
+l = lambda x : x + 2
+print(l(3))
+b = list(map(l, a))
+b = list(map(lambda x : x + 2, a))
+print(b)
