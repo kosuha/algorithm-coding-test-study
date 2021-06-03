0606327aa794e778e429a47a410f88eaaf00cd33
Seonho Kim <okeydokekim@gmail.com>
2021-06-03 12:03:14 +0900

    210603

--- lecture/test.py
+++ lecture/test.py
@@ -1,20 +1,20 @@
 ### Section 1 ###
 
 # 1부터 n까지 홀수 출력
-def a(n):
+def f1(n):
     for i in range(1, n + 1):
         if i % 2 == 1:
             print(i)
 
 # 1부터 n까지의 합 구하기
-def b(n):
+def f2(n):
     result = 0
     for i in range(1, n + 1):
         result += i
     print(result)
 
 # n의 약수 출력하기
-def c(n):
+def f3(n):
     for i in range(1, n + 1):
         if n % i == 0:
             print(n // i)
@@ -22,7 +22,7 @@ def c(n):
 ### Section 2 ###
 
 # k번째 약수
-def d(n, k):
+def f4(n, k):
     divisors = []
     for i in range(1, n + 1):
         if n % i == 0:
@@ -33,4 +33,25 @@ def d(n, k):
     else:
         return divisors[k - 1]
 
-print(d(6, 5))
+# k번째 수
+def f5(n, s, e, k):
+    l = n[s - 1 : e]
+    l.sort()
+    print(l)
+    return l[k - 1]
+
+# k번째 큰 수
+def f6(n, k):
+    l = list(map(int, n.split(" ")))
+    print(l)
+    sums = [] # sums = set()
+    for i in range(0, len(l) - 2):
+        for ii in range(i + 1, len(l) - 1):
+            for iii in range(ii + 1, len(l)):
+                sums.append(l[i] + l[ii] + l[iii]) # sums.add(l[i] + l[ii] + l[iii])
+    
+    sums = list(set(sums)) # sums를 처음부터 set으로 선언해도 된다.
+    sums.sort()
+
+    return sums[-k]
+
